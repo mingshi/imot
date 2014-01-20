44ff16e58b5cfa6a899c8289b4f9f260c4f8d9ad
mingshi <fivemingshi@gmail.com>
2014-01-20 13:45:27 +0800

    init Scaffold

---
+++ lib/Mot/IMOT.pm
@@ -0,0 +1,21 @@
+#
+#===============================================================================
+#
+#         FILE: IMOT.pm
+#
+#  DESCRIPTION: 
+#
+#        FILES: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: Mingshi (deepwarm.com), fivemingshi@gmail.com
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 2014/01/20 13时42分34秒
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+ 
+
